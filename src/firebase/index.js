0c3959c3d39f92e7b2c12cfae766632611f349de
Jefferson1LL <jefferson.rllumiquinga93@gmail.com>
2021-03-12 19:58:40 -0500

    firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+const firebaseConfig = {
+    apiKey: "AIzaSyB6f8q-bjq-OdZVzi3_GLVswfbV3JcQKYI",
+    authDomain: "proyecto-ddi.firebaseapp.com",
+    projectId: "proyecto-ddi",
+    storageBucket: "proyecto-ddi.appspot.com",
+    messagingSenderId: "168124626246",
+    appId: "1:168124626246:web:d4495a3809c0c1e7c7a147",
+    measurementId: "G-H1TM0BSZXD"
+};
+const app = firebase.initializeApp(firebaseConfig);
