2f266602abb77eb88750830f40dfd630a4f8b888
Jorge Alba <jorge.alba@epn.edu.ec>
2021-03-14 09:38:02 -0500

    Mejoramiento diseño

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -5,13 +5,13 @@ import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
-    <Result
-      status="404"
-      title="404"
-      subTitle="Estás pérdido? vuelve a casa."
+    <Result 
+      status="500"
+      title="500"
+      subTitle="Lo sentimos, algo salio mal."
       extra={
         <Button type="primary">
-          <Link to={Routes.HOME}>Ir a inicio</Link>
+          <Link to={Routes.HOME}>Regresar a Inicio</Link>
         </Button>
       }
     />
